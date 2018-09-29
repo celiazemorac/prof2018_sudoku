87d20d519f0ec9f9f1cfd7c25c93daab63c99e31
celiazemorac <zemorac94@gmail.com>
2018-09-29 23:00:12 +0200

    Added .gitignore file

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,7 +1,6 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-public class SudokuVerifier 
-{
+public class SudokuVerifier {
 	public int verify (String candidateSolution)
     {
         return 0;
