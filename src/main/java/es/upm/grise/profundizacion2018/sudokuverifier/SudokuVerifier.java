5bdd0588e3b04af4b213d107b5fc7acf418da62f
celiazemorac <zemorac94@gmail.com>
2018-09-29 23:07:30 +0200

    Revert "Added .gitignore file"
    This reverts commit 87d20d519f0ec9f9f1cfd7c25c93daab63c99e31.

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,6 +1,7 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-public class SudokuVerifier {
+public class SudokuVerifier 
+{
 	public int verify (String candidateSolution)
     {
         return 0;
